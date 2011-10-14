699e08ae320b2099d229405c463a2b0f97d2c300
Jonathan Sharp <jonathan@jdsharp.com>
2011-10-13 19:35:51 -0500

    More tweeks with a display of the url for redirect

--- go.php
+++ go.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL);
 function lookup($key) {
 	foreach (file('urls.txt') as $line_num => $line) {
 		$line = trim($line);
-		$eq   = strpos('=', $line);
+		$eq   = strpos($line, '=');
 		$k    = substr($line, 0, $eq);
 		$v    = substr($line, $eq+1);
 
@@ -18,4 +18,5 @@ preg_match('/\/go\/(.*)/', $_SERVER['REQUEST_URI'], $match);
 if (!isset($match[1])) {
 	die("Missing parameter, call via /go/[key]");
 }
-header('Location: ' . lookup($match[1]));
+$url = lookup($match[1]);
+echo 'Redirecting to: <a href="' . $url . '">' . $url . '</a><meta http-equiv="Refresh" content="5; URL=' . $url . '">';
